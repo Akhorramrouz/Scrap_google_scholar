244997971d15ad88aa8c405485008f052421d1be
Adel <ak8480@rit.edu>
2023-08-02 10:14:47 -0400

    Update advisor.py

--- advisor.py
+++ advisor.py
@@ -23,7 +23,9 @@ class Advisor:
         for co_author in self.driver.find_elements(By.CLASS_NAME, "gsc_rsb_aa"):
             co_author_name = BeautifulSoup(co_author.get_attribute("outerHTML"),'html.parser').find("span", class_="gsc_rsb_a_desc").find('a').text
             co_author_scholar_link = self.get_coauthor_link(co_author.get_attribute("outerHTML"))
-            coauthors.append((co_author_name, co_author_scholar_link))
+            coauthors.append({"co_author_name":co_author_name,
+                               "co_author_scholar_link":co_author_scholar_link})
+            
         return coauthors
     
     
